9d6fe16d716be0a54f38224c33fb91b41837311a
Ahmad Aqil <zyyrexi0@gmail.com>
2025-07-26 10:53:39 +0700

    modified:   calculator.html 	modified:   script.js

--- script.js
+++ script.js
@@ -6,32 +6,34 @@ const inputSC = document.getElementById("inputSC");
 const submitSC = document.getElementById("submitSC");
 let linkValue = "";
 let secretCd = "";
+let clickCount = 0;
 
 // container hidden
-const containerInput = document.getElementById("containerInput")
-const calculatorCon = document.getElementById("calculator-container")
-const test = document.getElementById("test")
-let clickCount = 0
-// event listener for hidden object
-document.body.addEventListener("click", function(event) {
-  if (calculatorCon.contains(event.target)) return;
+const containerInput = document.getElementById("containerInput");
+const calculatorCon = document.getElementById("calculator-container");
+document.body.addEventListener("click", function (event) {
+  if (calculatorCon.contains(event.target) || containerInput.contains(event.target) || display.contains(event.target)) return;
   // increase click count
-  clickCount++
+  clickCount++;
   // set display hidden if tap 3 times
   if (clickCount == 3) {
-    containerInput.style.display = "block"
+    containerInput.style.display = "block";
+  }
+  // hide and display it again
+  if (clickCount == 6) {
+    containerInput.style.display = "none";
     clickCount = 0
   }
-  // containerInput.style.display = "block"
-}
-)
+});
 // button link input
 submitLink.addEventListener("click", function () {
-  linkValue = inputLink.value;
-  inputLink.value = "";
+  linkValue = inputLink.value.trim();
+  if (!linkValue.startsWith("http://") && !linkValue.startsWith("https://")){
+    linkValue = "https://" + linkValue
+  }
 });
 
-// button SecretCode input 
+// button SecretCode input
 submitSC.addEventListener("click", function () {
   secretCd = inputSC.value;
   inputSC.value = "";
@@ -57,14 +59,13 @@ function clearDisplay() {
 let result = "";
 function calculate() {
   try {
-  let expression = display.value.replace(/×/g, "*").replace(/÷/g, "/");
-  display.value = eval(expression);
-  }
-  catch (error) {
+    let expression = display.value.replace(/×/g, "*").replace(/÷/g, "/");
+    display.value = eval(expression);
+  } catch (error) {
     display.value = "Error";
   }
- 
-  result = display.value
+
+  result = display.value;
   if (result === secretCd) {
     window.open(linkValue);
   }
